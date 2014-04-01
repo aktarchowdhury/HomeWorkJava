fd89c4c5401b0da6bd6ccc2ecc97b5684b3821f9
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-03-31 21:59:56 -0400

    Switch Java Class

---
+++ SwitchClass.java
@@ -0,0 +1,26 @@
+package com.aktar.switching;
+
+public class SwitchClass {
+
+	public static void main(String[] args) {
+		int tuna;
+		tuna = 4;
+		switch (tuna) {
+		case 1:
+			System.out.println("Drop in the sea");
+			break;
+		case 2:
+			System.out.println("take in the boat");
+			break;
+		case 3:
+			System.out.println("cook it for Lunch");
+		case 4:
+			System.out.println("I donot know what to do");
+			break;
+			
+
+		}
+
+	}
+
+}
