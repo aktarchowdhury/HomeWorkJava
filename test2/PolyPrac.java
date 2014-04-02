3b5f66c45873b93116984c7b87b6100af5cf771c
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-02 15:45:47 -0400

    Method Overriding In Java

---
+++ test2/PolyPrac.java
@@ -0,0 +1,18 @@
+package com.test2;
+
+public class PolyPrac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+add(3,3);
+add(3,3,3);
+	}
+public static void add(int a, int b){
+	int result=a+b;
+	System.out.println("Result is"+result);
+}
+public static void add(int a, int b, int c){
+	int result=a+b+c;
+	System.out.println("Result is"+result);
+}
+}
